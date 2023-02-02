dd81030cfe416fa55f8600055ab2de1210cf214e
tvinente <vinente.talyta@gmail.com>
2023-02-02 16:21:59 -0300

    Desafio de código DIO, calcular mesada em X meses.

---
+++ src/MesadaSobrinho.java
@@ -0,0 +1,20 @@
+  // Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:  
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next"; 
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.   
+
+import java.util.Scanner; 
+    
+public class MesadaSobrinho {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);        
+        int mesada = 50;
+        
+        // TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro irá juntar nos meses da entrada
+
+        System.out.print("Digite a quantidade de meses: ");
+        int entrada = leitor.nextInt();
+        int saida = entrada * mesada;
+        System.out.println("Voce tera " + saida + " reais");
+       
+    }  
+}
